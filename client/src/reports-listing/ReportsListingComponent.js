7455f5685a8a33df0d5e03aa40fa89e2c7f7304f
Islam Gad <i_gad_@hotmail.com>
2018-10-16 18:05:45 +0100

    Create fronted components
    Co-authored-by: Ada Solcan <asolcan@pivotal.io>

--- client/src/reports-listing/ReportsListingComponent.js
+++ client/src/reports-listing/ReportsListingComponent.js
@@ -1,14 +1,28 @@
 import React, { Component, Fragment } from "react";
-import { Grid, Paper, Typography, withStyles } from "@material-ui/core";
+import {
+  Grid,
+  Paper,
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableRow,
+  Typography,
+  withStyles
+} from "@material-ui/core";
 import HeaderComponent from "../page-layout/HeaderComponent";
 
 import type { Account } from "../authentication/models";
+import type { Report } from "../report/models";
 
 type Props = {
   classes: any,
   logout: () => void,
-  account: ?Account
+  account: ?Account,
+  reports: ?(Report[])
 };
+
+const moment = require("moment");
 
 const styles = theme => ({
   rowContainer: {
@@ -26,8 +40,62 @@ const styles = theme => ({
 });
 
 export class ReportsListingComponent extends Component<Props> {
+  noReportsMessage(classes: any) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography
+          data-test-id="no-reports-title"
+          variant="h5"
+          className={classes.paperTitle}
+        >
+          No submitted reports yet!
+        </Typography>
+        <Typography data-test-id="no-reports-message">
+          Once you’ve a completed report it will appear here.
+        </Typography>
+      </Paper>
+    );
+  }
+
+  reportsTable(classes: any, reports: Report[]) {
+    return (
+      <Paper className={classes.root}>
+        <Table data-test-id="submitted-reports" className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>GRANT</TableCell>
+              <TableCell>PERIOD</TableCell>
+              <TableCell>SUBMITTED</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {reports.map((report: Report) => {
+              return (
+                <TableRow data-test-id="report" key={report.id}>
+                  <TableCell data-test-id="report-grant">
+                    {report.grant}
+                  </TableCell>
+                  <TableCell data-test-id="report-period">
+                    {moment(report.reportPeriod).format("MMMM YYYY")}
+                  </TableCell>
+                  <TableCell data-test-id="report-submitted">
+                    {moment(report.submissionDate).format("DD/MM/YYYY")}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+
   render() {
-    const { classes, logout, account } = this.props;
+    const { classes, logout, account, reports } = this.props;
+    const content
+      = reports && reports.length > 0
+        ? this.reportsTable(classes, reports.filter(report => report.completed))
+        : this.noReportsMessage(classes);
     return (
       <Fragment>
         <HeaderComponent logout={logout} account={account} />
@@ -39,23 +107,10 @@ export class ReportsListingComponent extends Component<Props> {
             </Typography>
           </Grid>
         </Grid>
-
+        {content}
         <Grid container className={classes.rowContainer}>
           <Grid item xs={1} />
-          <Grid item xs={10}>
-            <Paper className={classes.paper}>
-              <Typography
-                data-test-id="no-reports-title"
-                variant="h5"
-                className={classes.paperTitle}
-              >
-                No submitted reports yet!
-              </Typography>
-              <Typography data-test-id="no-reports-message">
-                Once you’ve a completed report it will appear here.
-              </Typography>
-            </Paper>
-          </Grid>
+          <Grid item xs={10} />
         </Grid>
       </Fragment>
     );
--- client/src/reports-listing/ReportsListingComponent.test.js
+++ client/src/reports-listing/ReportsListingComponent.test.js
@@ -33,4 +33,64 @@ describe("ReportsListingComponent", () => {
       ).toContain("Once you’ve a completed report it will appear here.");
     });
   });
+
+  describe("with reports", () => {
+    const reports = [
+      {
+        id: 1,
+        completed: true,
+        overview: "Mitchell Overview completed",
+        grant: "Grant Mitchell",
+        reportPeriod: "2018-10-01T00:00:00.000Z",
+        submissionDate: "2018-09-15T03:24:00.000Z"
+      },
+      {
+        id: 2,
+        completed: false,
+        overview: "Jone Overview",
+        grant: "Jone Grace",
+        reportPeriod: "2018-09-01T00:00:00.000Z"
+      }
+    ];
+    beforeEach(() => {
+      wrapper = shallow(
+        <ReportsListingComponent
+          classes={{}}
+          account={undefined}
+          logout={() => {}}
+          reports={reports}
+        />
+      );
+    });
+
+    it("displays a list of completed reports", () => {
+      expect(
+        wrapper.find(
+          '[data-test-id="submitted-reports"] [data-test-id="report"]'
+        )
+      ).toHaveLength(1);
+      const firstReport = wrapper
+        .find('[data-test-id="submitted-reports"] [data-test-id="report"]')
+        .first();
+      expect(
+        firstReport
+          .find('[data-test-id="report-grant"]')
+          .render()
+          .text()
+      ).toContain(reports[0].grant);
+
+      expect(
+        firstReport
+          .find('[data-test-id="report-period"]')
+          .render()
+          .text()
+      ).toContain("October 2018");
+      expect(
+        firstReport
+          .find('[data-test-id="report-submitted"]')
+          .render()
+          .text()
+      ).toContain("15/09/2018");
+    });
+  });
 });
