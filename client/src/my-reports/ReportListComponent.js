c4c5934c805ad9d37b948b628ef3459d90783c50
Jonathan Sharpe <jsharpe@pivotal.io>
2018-10-18 11:17:44 +0100

    Split up report list into smaller rendering methods

--- client/src/my-reports/ReportListComponent.js
+++ client/src/my-reports/ReportListComponent.js
@@ -14,25 +14,31 @@ export default (props: Props) => (
   <div>
     {props.reports.map(
       (report, index) =>
-        report.completed ? (
-          <ReportCardComponent
-            report={report}
-            key={index}
-            updateReport={props.updateReport}
-          />
-        ) : (
-          <Link
-            key={report.id}
-            to={`/reports/${report.id}`}
-            style={{ textDecoration: "none" }}
-          >
-            <ReportCardComponent
-              report={report}
-              key={index}
-              updateReport={props.updateReport}
-            />
-          </Link>
-        )
+        report.completed
+          ? completeReport(report, index, props.updateReport)
+          : incompleteReport(report, index, props.updateReport)
     )}
   </div>
+);
+
+const incompleteReport = (report, index, updateReport) => (
+  <Link
+    key={report.id}
+    to={`/reports/${report.id}`}
+    style={{ textDecoration: "none" }}
+  >
+    <ReportCardComponent
+      report={report}
+      key={index}
+      updateReport={updateReport}
+    />
+  </Link>
+);
+
+const completeReport = (report, index, updateReport) => (
+  <ReportCardComponent
+    report={report}
+    key={index}
+    updateReport={updateReport}
+  />
 );
